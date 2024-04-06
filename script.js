8cd58fddef59b863b6845fdbf6563d405bdcc01b
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-04-06 19:30:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(private species: string) {}
 
-class Dog extends Animal {}
+    getSpecies(): string {
+        return this.species;
+    }
 
-class Cat extends Animal {}
+    makeSound(): void {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
+class Cat extends Animal {
+    purr(): void {
+        console.log('purr');
+    }
+}
+
+class Dog extends Animal {
+    bark(): void {
+        console.log('woof');
+    }
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
